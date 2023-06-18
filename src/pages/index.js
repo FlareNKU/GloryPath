c59aa0a324b12fdbda59b596c612d032b0eaccf1
YwFalcon <ywflare@gmail.com>
2023-06-18 20:12:25 +0800

    update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`荣耀之路`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
